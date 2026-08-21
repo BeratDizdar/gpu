0900f454268263d17b894dd82bc5bd1bcdba239f
BeratDizdar <beratdizdar2004@gmail.com>
2026-08-21 03:11:12 +0300

    açıklama yazısı ekledim

--- gpu.h
+++ gpu.h
@@ -1,13 +1,85 @@
-/*
-- KESKIN bir kural kümesi olmayacak.
-- Amaç ortak noktada buluşturmak. Bunun için farklı teknikler kullanılabilir.
-- Sonuçta olay veri gönder, veri işle[, veri al] olduğu için kolaylık olmalı.
-- Bu konuda implemente edene sorumluluk düşecek.
-- Referans olarak kendi resmi implementasyonlarım olacak.
-- 2 farklı Vulkan implementasyonu farklı teknikler kullanıp farklı performanslar
-verebilecek.
-- Biraz driver yaklaşımı gibi düşünülebilir.
-*/
+/* IGPUDevice ==================================================================
+    Implementasyona dayalı bir api yaratmak istedim. Eğer benim yarattığım 
+referans niteliğindeki yapısı sevmezseniz kendiniz yapmakta özgürsünüz.
+Kesin bir kural kümesi olmayacak. Farklı teknikler kullanılabilir. Sonuçta
+olay veri gönder, işle[, al] olduğu için kolaylık olmalı. Haliyle 2 farklı
+Vulkan implementasyonu farklı teknikler kullanıp farklı performanslar verebilir.
+    `New*` ile yarattığımız yapılar için `Delete*` gibi varyasyonların olmama 
+sebebi heap kullanımına bu konularda karşı olmam. Bütün API boyunca kendi 
+fikrimi dayattığım tek konu bu olacak.
+    Herkese kolay gelsin.
+
+* PLAN *
+    * Henüz renderbuffer, framebuffer gibi şeyler eklemeyeceğim. 
+    * Stencil, depth falan da zira basit tutmak istiyorum şimdilik.
+    * Eğer gerekli olduğunu hissedersem direkt ekleyeceğim.
+    * NewTexture için veri tipi istemeli.
+    * QueryProps için sorgu sabitleri olmalı.
+
+INFO ---------------------------------------------------------------------------
+
+- QueryProps -> Donanım ile ilgili bilgiler
+
+PIXEL-TEST ---------------------------------------------------------------------
+
+- Scissor -> Piksel başına kesme testi
+
+COORDINATE ---------------------------------------------------------------------
+
+- Viewport -> Görüş alanı ayarı
+
+TEXTURE ------------------------------------------------------------------------
+
+- NewTexture:
+    Belirli doku slotuna yeni doku ekle
+    Yeni doku eklemek eskisinin temizlenmesi anlamına gelir
+    Doku formatları için sabitler:
+    - [0, 3]   = {RGBA8, RGBA4, RGB5_A1, RGBA2}
+    - (3, ???] = impl extension
+- BindTexture: 
+    Belirli doku slotunu donanım için aktif birime koyar
+    Filtre formatları için sabitler:
+    - [0, 1]   = {NEAREST, LINEAR}
+    - (1, ???] = impl extension
+
+BUFFER -------------------------------------------------------------------------
+
+- NewBuffer:
+    Belirli veri slotuna yeni veri ekle
+    Yeni veri eklemek eskisinin temizlenmesi anlamına gelir
+- UpdateBuffer -> Belirli veri slotundaki veriyi güncelle
+- BindBuffer -> Belirli doku slotunu donanım için aktif birime koyar
+
+PIPELINE -----------------------------------------------------------------------
+    Shader program işleme kısmını tek bir pipeline yapısında planladım. Hangi 
+program tipini kullanacağınız implementasyona kalsa bile benim tercihim, verilen
+`shader_count`a göre comp veya vert+frag şeklinde işlenmesi yönünde.
+    Nihai çözüm için aklımda bir "ShaderIR" yaratmak. Fakat şimdilik böyle 
+bırakıyorum. :P
+
+- NewPipeline:
+    Belirli pipeline slotuna yenisini ekle
+    Yeni pipeline eklemek eskisinin temizlenmesi anlamına gelir
+- CurrentPipeline -> Seçili pipeline slotunu aktif et
+
+COMMAND ------------------------------------------------------------------------
+
+- Draw:
+    Çizim komutunu gönder (vert+frag pipeline)
+    Çizim topolojileri için sabitler:
+    - [0, 4]   = {POINT, LINE, LINE_STRIP, TRIANGLE, TRIANGLE_STRIP}
+    - (4, ???] = impl extension
+- Compute -> İşleme komutunu gönder (comp pipeline)
+- MemoryBarrier -> VRAM veri yarışını engeller
+
+FRAMEBUFFER --------------------------------------------------------------------
+
+- Clear -> Aktif yüzeyi temizle
+- Present:
+    İşlenen yüzeyi ekrana bas
+    OpenGL için gerekli değil
+
+==============================================================================*/
 
 #pragma once
 #include <stdint.h>
@@ -27,55 +99,21 @@ typedef struct { // devamını düşüneceğim bir ara
 #ifdef __cplusplus
 
 struct IGPUDevice {
-    // henüz renderbuffer, framebuffer gibi şeyler eklemeyeceğim. 
-    // Stencil, depth falan da zira basit tutmak istiyorum şimdilik.
-    // eğer gerekli olduğunu hissedersem direkt ekleyeceğim.
+    
     virtual const char *QueryProps() = 0;
-
-    // basit yapılar işte
     virtual void Scissor(int x, int y, int w, int h) = 0;
     virtual void Viewport(int x, int y, int w, int h) = 0;
     virtual void Clear(uint8_t r, uint8_t g, uint8_t b) = 0;
-
-    // format tipleri olarak unsigned int mi kullanmalıyım yoksa formata göre
-    // değişiyor mu???
-    // format:
-    // [0, 3]   = {RGBA8, RGBA4, RGB5_A1, RGBA2}
-    // (3, ???] = impl extension
     virtual void NewTexture(int id, int format, int w, int h, const void *data) = 0;
-    // filter:
-    // [0, 1]   = {NEAREST, LINEAR}
-    // (1, ???] = impl extension
     virtual void BindTexture(int id, int slot, int filter) = 0;
-
-    // data float* veriliyor varsayılıyor şuanlık
-    // merak etmeyin çok hızlı değiştireceğim sonrasında bu durumu
     virtual void NewBuffer(int id, size_t size, const void *data) = 0;
     virtual void UpdateBuffer(int id, size_t offset, size_t size, const void *data) = 0;
     virtual void BindBuffer(int id, int slot) = 0;
-
-    // ve burada da implemente edenler istediği shaderi arkada impl etsinler
-    // diye böyle yaptım.
-    // software port içinde aklımda bir VM yazıp üzerinde çalıştırmak var.
-    // evet impl kısmına bıraktım. bunun sıkıntı bir sonucu var impl karar verecek
-    // ne olduğuna, bu hem iyi hem kötü.
-    // nihai çözümü bu apiye özgü bir SHADER IR olması. fakat henüz yapamam.
-    // bu nedenle şuanlık böyle olması yeterli
     virtual void NewPipeline(int id, int shader_count, void **shader) = 0;
     virtual void CurrentPipeline(int id) = 0;
-
-    // normalde bu execute içinde type'ın tipi topology_t olacaktı fakat int
-    // demeyi daha uygun gördüm zira implemente edenler desteğine göre içeride
-    // planlayıp dışarıya bunu söyleyebilir
-    // count parametresi aslında vertex_id gibi olacak
-    // type:
-    // [0, 4]   = {POINT, LINE, LINE_STRIP, TRIANGLE, TRIANGLE_STRIP}
-    // (4, ???] = impl extension
     virtual void Draw(int type, int count) = 0;
     virtual void Compute(int num_groups_x, int num_groups_y, int num_groups_z) = 0;
     virtual void MemoryBarrier() = 0;
-
-    // opengl ise zaten hatayı kendi içinde verir.
     virtual void Present() = 0;
 };
 
